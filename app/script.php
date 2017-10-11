940e38d0e4e01494258a9f5bf31ef2e25b662848
grayni <omenbestg@gmail.com>
2017-10-11 13:58:52 -0700

    pad(script.php) development and test

--- app/script.php
+++ app/script.php
@@ -1,43 +1,22 @@
-<?php 
-	// $data = array(
-	// 	'name' => 'Steven',
-	// 	'email' => 'steve@junkie.com',
-	// 	'phone' => '123-45-67',
-	// 	'message' => 'Hello. I want to buy some weed.'
-	// );
+<?php
+	$long_link = $_POST["long-link"];
+	$fantasy = $_POST["fantasy-link"];
 
-	// echo json_encode($data);
+	// $ch = curl_init($long_link);
 
-	//print_r ($_POST);
-/*Order*/
-// 1)Test form  on long and fantazy link together
-// if true = send request in data-base on compare identical 
-// 1.1) create unique key in data-base 
-// 		if exist this return short this 
-// fantasy_link test in two tables (auto_link , fantasy_link)
-	// if ($_POST['long-link'] && $_POST['fantasy-link']) {
+	// curl_exec($ch);
 
-	// 	header("Location:/");
+
+	// if (!curl_errno($ch)) {
+	//   $info = curl_getinfo($ch);
+	//   echo 'Прошло ', $info['total_time'], ' секунд во время запроса к ', $info['url'], "\n";
 	// }
-	// else if (!$_POST['fantasy-link']) {
-	// 	echo "Заполните поле2";
-	// }
-	// 
-	// существование переменной
-	// $x = 15;
-	// if (isset($x)) {
-	// 	echo 'Перемення существует!';
-	// }
-	// else {
-	// 	echo "Такой переменной нет";
-	// }
-	// Вывод строки пути файла, из которого вызывается данный код
-	// echo __FILE__."<br>";
-	// // Подготовка файла к изменению прав
-	// echo fileperms(__FILE__);
-	// // смена прав
-	// chmod(__FILE__, 0777);
-	// Инфо по серверу
-	// phpinfo();
-	//echo $_SERVER['REMOTE_ADDR'];
-	phpinfo();
+
+	// curl_close($ch);
+
+	if ($_POST["enter"]) {
+		echo "Длинная ссылка: $long_link<br>";
+		echo "Придуманная ссылка: $fantasy<br>";
+		echo "test string";
+	}
+?>
