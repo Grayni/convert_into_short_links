90d3841d9e13a691bb0b687fbc6f73a044c49361
grayni <omenbestg@gmail.com>
2017-10-14 05:33:42 +0300

    pad(ajax.js) finish ajax.js

--- app/js/ajax.js
+++ app/js/ajax.js
@@ -1,27 +1,60 @@
-/*$('document').ready(function() {
+function showNewLink () {
 
-    // for old explore
+    // test browser
     function getXMLHttpRequest() {
+
         if (window.XMLHttpRequest) {
             return new XMLHttpRequest();
         }
         return new ActiveXObject('Microsoft.XMLHTTP');
+
     }
 
-    request = getXMLHttpRequest();
+    var request = getXMLHttpRequest();
 
-    // require status query
     request.onreadystatechange = function() {
-            if (request.readyState == 4) {
-                var responseBody = request.responseText,
-                    data = JSON.parse(responseBody);
-                $('.btn-success').on('click', function() {
-                    $('.card-body').append(data['.message']);
+
+        let
+            copy = $('#copy-click'),
+            resp = $('#responce'),
+            card = $('.card');
+
+        if (request.readyState == 4 && request.status == 200) {
+
+            card.animate({'height': '300px'}, function() {
+
+                card.animate({'height': '340px'}, function() {
+
+                    resp.html(request.responseText).css('display', 'flex').hide().fadeIn(200);
+
+                    if ($('#new-link').length>0) {
+                        copy.fadeIn(300);
+                    }
+                    $('.ajax-loader').hide();
+
                 });
-            }
+            });
+        }
     };
 
-    request.open('GET', '../script.php', true);
-    request.send(null);
+    let params = ['long-link','link-default'];
 
-});*/
+    function inputId(a) {
+        return document.getElementById(a);
+    }
+
+    // note: encode - against conflict (&) params&params => server
+    params = params.map(L => inputId(L).name + '=' + encodeURIComponent(inputId(L).value));
+    params = params.join('&');
+
+    $('#long-link').keyup(function() {
+      console.log(params);
+    });
+
+    request.open('POST', 'script', true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.send(params);
+}
+
+
+
